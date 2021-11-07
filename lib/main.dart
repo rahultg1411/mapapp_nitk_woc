f7044da8c2262d34ad4e46707a92f87d3056a720
Rahul <rahulgonchikar1411@gmail.com>
2021-11-08 02:09:10 +0530

    small modification

--- lib/main.dart
+++ lib/main.dart
@@ -45,27 +45,52 @@ class HomePage extends StatelessWidget {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'Welcome to NITK',
-                style: TextStyle(
-                  color: Color.fromRGBO(207, 14, 58, 1.0),
-                  fontSize: 43,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold,
-                  backgroundColor: Color.fromRGBO(7, 41, 22, 0.8),
+              Container(
+                padding: EdgeInsets.all(1),
+                alignment: Alignment.center,
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(58, 134, 9, 0.6509803921568628),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    width: 5,
+                    color: Color.fromRGBO(186, 12, 93, 1.0),
+                    style: BorderStyle.solid,
+                  ),
+                ),
+                child: const Text(
+                  'Welcome to NITK!',
+                  style: TextStyle(
+                    color: Color.fromRGBO(10, 31, 155, 1.0),
+                    fontSize: 40,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               const SizedBox(
-                width: 200.0,
-                height: 300.0,
+                height: 290.0,
               ),
-              const Text(
-                'Explore NITK',
-                style: TextStyle(
-                  color: Colors.lightGreenAccent,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  backgroundColor: Colors.black87,
+              Container(
+                padding: EdgeInsets.all(1),
+                alignment: Alignment.center,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(0, 0, 0, 0.6509803921568628),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    width: 3,
+                    color: Color.fromRGBO(11, 203, 221, 1.0),
+                    style: BorderStyle.solid,
+                  ),
+                ),
+                child: const Text(
+                  'Explore NITK!',
+                  style: TextStyle(
+                    color: Colors.lightGreenAccent,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ],
